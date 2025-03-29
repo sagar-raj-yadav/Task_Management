fb0560e7dbcc46d28a9586d2e632cc628829b911
sagar raj yadav <115137591+sagar-raj-yadav@users.noreply.github.com>
2025-03-29 21:54:53 +0530

    Update TaskCard.jsx

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const TaskCard = ({ task, onEdit, onDelete }) => {
     return (
         <div className="task-card">
-            <h3 className="task-title">Title:{task.title}</h3>
-            <p className="task-description">description{task.description}</p>
+            <h3 className="task-title"><strong>Title:</strong> {task.title}</h3>
+            <p className="task-description"><strong>Description:</strong> {task.description}</p>
             <p className="task-details"><strong>Due Date:</strong> {task.dueDate}</p>
             <p className="task-details"><strong>Assigned User:</strong> {task.assignedUser}</p>
             <p className="task-details"><strong>Priority:</strong> {task.priority}</p>
             <p className="task-details"><strong>Status:</strong> {task.status}</p>
             <div className="task-buttons">
-                <button className="edit-button" onClick={onEdit}>Edit</button>
-                <button className="delete-button" onClick={onDelete}>Delete</button>
+                <button className="edit-button" onClick={() => onEdit(task._id)}>Edit</button>
+                <button className="delete-button" onClick={() => onDelete(task._id)}>Delete</button>
             </div>
         </div>
     );
